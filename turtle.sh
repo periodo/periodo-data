021d78c74e2a465dbde9cea7a7448ef3c1dac39d
Ryan Shaw <ryanshaw@unc.edu>
2014-10-11 11:40:03 -0400

    abbreviate URIs in turtle output

--- turtle.sh
+++ turtle.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 
-#DATA="https://raw.githubusercontent.com/periodo/examples/master/data.json"
 DATA="data.json"
 BASE_URI="http://perio.do/temporary/"
 
-jsonld normalize -q $DATA | rapper -i nquads -o turtle - $BASE_URI
+jsonld normalize -q $DATA | rapper -i nquads -o turtle \
+                                   -f 'xmlns:skos="http://www.w3.org/2004/02/skos/core#"' \
+                                   -f 'xmlns:dcterms="http://purl.org/dc/terms/"' \
+                                   -f 'xmlns:foaf="http://xmlns.com/foaf/0.1/"' \
+                                   -f 'xmlns:time="http://www.w3.org/2006/time#"' \
+                                   -f 'xmlns:xsd="http://www.w3.org/2001/XMLSchema#"' \
+                                   -f 'xmlns:periodo="http://perio.do/temporary/"' \
+                                   - $BASE_URI
