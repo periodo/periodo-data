98e93f1c1be086f9432f57060af970156e2733e1
Ryan Shaw <ryanshaw@unc.edu>
2015-01-27 14:37:50 -0500

    Assigned identifiers.

--- turtle.sh
+++ turtle.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
-DATA="data.json"
-BASE_URI="http://perio.do/temporary/"
+BASE_URI="http://n2t.net/ark:/99152/p0/"
 
-jsonld normalize -q $DATA | rapper -i nquads -o turtle \
+jsonld normalize -q $1 | rapper -i nquads -o turtle \
+                                   -f 'xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"' \
                                    -f 'xmlns:bibo="http://purl.org/ontology/bibo/"' \
                                    -f 'xmlns:dcterms="http://purl.org/dc/terms/"' \
                                    -f 'xmlns:foaf="http://xmlns.com/foaf/0.1/"' \
@@ -11,5 +11,6 @@ jsonld normalize -q $DATA | rapper -i nquads -o turtle \
                                    -f 'xmlns:skos="http://www.w3.org/2004/02/skos/core#"' \
                                    -f 'xmlns:time="http://www.w3.org/2006/time#"' \
                                    -f 'xmlns:xsd="http://www.w3.org/2001/XMLSchema#"' \
-                                   -f 'xmlns:periodo="http://perio.do/temporary/"' \
-                                   - $BASE_URI
+                                   -f "xmlns:periodo=\"${BASE_URI}vocab#\"" \
+                                   - "$BASE_URI"
+
