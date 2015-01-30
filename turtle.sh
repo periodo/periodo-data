e655afaff1c208f3e3b94ea485cb6a08305a90ed
Ryan Shaw <ryanshaw@unc.edu>
2015-01-30 15:54:01 -0500

    Convert to flat identifiers.

--- turtle.sh
+++ turtle.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-BASE_URI="http://n2t.net/ark:/99152/p0/"
+BASE_URI="http://n2t.net/ark:/99152/"
 
 jsonld normalize -q $1 | rapper -i nquads -o turtle \
                                    -f 'xmlns:rdfs="http://www.w3.org/2000/01/rdf-schema#"' \
@@ -11,6 +11,6 @@ jsonld normalize -q $1 | rapper -i nquads -o turtle \
                                    -f 'xmlns:skos="http://www.w3.org/2004/02/skos/core#"' \
                                    -f 'xmlns:time="http://www.w3.org/2006/time#"' \
                                    -f 'xmlns:xsd="http://www.w3.org/2001/XMLSchema#"' \
-                                   -f "xmlns:periodo=\"${BASE_URI}vocab#\"" \
+                                   -f "xmlns:periodo=\"${BASE_URI}p0v#\"" \
                                    - "$BASE_URI"
 
